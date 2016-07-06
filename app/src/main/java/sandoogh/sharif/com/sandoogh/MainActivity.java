d1a249c6c6bd613c52354b140915105bcb0286c5
Shamim <sh.karimi94@gmail.com>
2016-07-06 22:43:17 +0430

    BaseActivity and MainActivity created. Navigation Drawer added in BaseActivity.

---
+++ app/src/main/java/sandoogh/sharif/com/sandoogh/MainActivity.java
@@ -0,0 +1,25 @@
+package sandoogh.sharif.com.sandoogh;
+
+import android.content.Context;
+import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.LayoutInflater;
+import android.view.View;
+
+public class MainActivity extends BaseActivity {
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        LayoutInflater inflater = (LayoutInflater) this.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+
+        View contentView = inflater.inflate(R.layout.activity_main, null, false);
+        mDrawerLayout.addView(contentView, 0);
+
+    }
+
+}
